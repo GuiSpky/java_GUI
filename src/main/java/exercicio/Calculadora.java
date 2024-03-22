ad112cff9f6b21450729868924958fae397142bb
Guilherme Fernandes de Souza <guii0410@gmail.com>
2024-03-22 14:14:38 -0300

    Feito a verificação dos campos com try-catch. Validando se o campo esta preenchido e se é com apenas valores numericos

--- src/main/java/exercicio/Calculadora.java
+++ src/main/java/exercicio/Calculadora.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 import static java.lang.Integer.parseInt;
+import static javax.swing.JOptionPane.showMessageDialog;
 
 public class Calculadora extends JFrame {
 
@@ -20,14 +21,25 @@ public class Calculadora extends JFrame {
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.insets = new Insets(5,5,5,5);
 
-        campoValor1 = new JTextField(10);
+        JLabel labelNome1 = new JLabel("Campo1:");
         constraints.gridx = 0;
         constraints.gridy = 0;
+        painel.add(labelNome1, constraints);
+
+        JLabel labelNome2 = new JLabel("Campo 2:");
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        painel.add(labelNome2, constraints);
+
+        campoValor1 = new JTextField(10);
+        constraints.gridx = 0;
+        constraints.gridy = 1;
         painel.add(campoValor1,constraints);
+
 
         campoValor2 = new JTextField(10);
         constraints.gridx = 1;
-        constraints.gridy = 0;
+        constraints.gridy = 1;
         painel.add(campoValor2,constraints);
 
         botaoSomar = new JButton("Somar");
@@ -42,16 +54,52 @@ public class Calculadora extends JFrame {
 
     }
 
-    private void somar(){
+
+    private void validarCampo1()throws NumberFormatException{
+        if (campoValor1.getText().isEmpty())
+            throw new RuntimeException("Campo 1 não pode ser vazio!");
+            var numero = Integer.parseInt(campoValor1.getText());
+
+//            throw new NumberFormatException("Campo 1 deve conter um valor numérico");
+
+    }
+
+    private void validarCampo2()throws NumberFormatException{
+        if (campoValor2.getText().isEmpty())
+            throw new RuntimeException("Campo 2 não pode ser vazio!");
+        var numero = Integer.parseInt(campoValor2.getText());
+
+    }
+    private void somar(){ // Feito 2 try-catch, um para validar cada campo
+        try{
+            validarCampo1();
+        }catch (NumberFormatException e){ //valida se o campo contem apenas numeros
+            showMessageDialog(this,
+                    "Campo 1 deve ser numerico");
+        }catch (RuntimeException re) { //valida se o campo esta vazio
+            showMessageDialog(this,
+                    re.getMessage());
+        }
+
+        try{
+            validarCampo2();
+        }catch (NumberFormatException e){ //valida se o campo contem apenas numeros
+            showMessageDialog(this,
+                    "Campo 2 deve ser numerico");
+        }catch (RuntimeException re) { //valida se o campo esta vazio
+            showMessageDialog(this,
+                    re.getMessage());
+        }
+
+
         int resultado;
-        int valor1;
-        int valor2;
-        valor1 = parseInt(campoValor1.getText());
-        valor2 = parseInt(campoValor2.getText());
+        int valor1 = parseInt(campoValor1.getText()); //pega os valres dos campos Jtext e passa para numerico
+        int valor2 = parseInt(campoValor2.getText());
         resultado = valor1 + valor2;
 //        System.out.println(resultado);
         JOptionPane.showMessageDialog(this,
                 valor1+" + "+valor2+" = "+resultado);
-
     }
+
+
 }
